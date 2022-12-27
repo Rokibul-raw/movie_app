49426569f7b8df9654ecd99f7b0d3d5719b34ef8
Rokibul Islam <mymainac2022@gmail.com>
2022-12-27 20:34:14 +0600

    Create react App and get Data from omdb api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import MovieCard from './Component/MovieCard';
+import SearchIcon from './search.svg'
+const API_URL='http://www.omdbapi.com/?i=tt3896198&apikey=522b6133'
+// const movie1={
+  
+//     "Title": "Spiderman and Grandma",
+//     "Year": "2009",
+//     "imdbID": "tt1433184",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg"
 
-function App() {
+// }
+const App=()=> {
+  const[movies,setMovies]=useState([]);
+  const[searchItem,setSearchitem]=useState('');
+  const searchMovie=async(title)=>{
+    const response= await fetch(`${API_URL}&s=${title}`); 
+    const data= await response.json();
+    setMovies(data.Search)
+  }
+  useEffect(()=>{
+    searchMovie('Spiderman')
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movie App</h1>
+      <div className='search'>
+        <input placeholder='Search your movie'
+        value={searchItem}
+        onChange={(e)=>setSearchitem(e.target.value)
+           
+        }/>
+
+        <img src={SearchIcon} alt='searchicon'
+        onClick={()=>searchMovie(searchItem)
+
+        } />
+
+        </div>
+        {movies?.length >0?
+          (<div className='container'>
+            {movies.map((movie)=>(
+              <MovieCard movie={movie}/>
+            ))}
+         </div>):(
+         <div className='empty'>
+         <h2>No Movie Here</h2>
+        </div>)
+         }
+
+      
+     
     </div>
+    
   );
 }
 
